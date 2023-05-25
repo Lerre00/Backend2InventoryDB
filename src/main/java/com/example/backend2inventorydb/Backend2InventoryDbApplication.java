525a469cc45904bd3d9479e07bc252c0a3c8dd7f
Leroy Collazo <leroycollazo@hotmail.com>
2023-05-25 13:22:16 +0200

    Initial commit

---
+++ src/main/java/com/example/backend2inventorydb/Backend2InventoryDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend2inventorydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend2InventoryDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend2InventoryDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend2inventorydb/Backend2InventoryDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend2inventorydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend2InventoryDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
