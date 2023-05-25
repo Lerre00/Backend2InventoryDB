fd5f5162d71e180cbd6bb84c6ccbbe5ba0b3a22b
Leroy Collazo <leroycollazo@hotmail.com>
2023-05-25 15:32:53 +0200

    third commit

--- sql/0002-test-data.sql
+++ sql/0002-test-data.sql
@@ -1,2 +1,6 @@
 INSERT INTO `item` VALUES (1,'Elias Egel', 999);
-UPDATE `item_seq` SET next_val = 51;
+INSERT INTO `item` VALUES (2,'Fotboll', 663);
+INSERT INTO `item` VALUES (3,'Basketboll', 426);
+INSERT INTO `item` VALUES (4,'Leksak', 254);
+INSERT INTO `item` VALUES (5,'Sko', 1235);
+UPDATE `item_seq` SET next_val = 55;
