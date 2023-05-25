17e9d0561551d35b3142697d0b5ef0a1dda625fd
Leroy Collazo <leroycollazo@hotmail.com>
2023-05-25 13:25:18 +0200

    second commit

--- src/main/java/com/example/backend2inventorydb/Backend2InventoryDbApplication.java
+++ src/main/java/com/example/backend2inventorydb/Backend2InventoryDbApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Backend2InventoryDbApplication {
-
+//hej
 	public static void main(String[] args) {
 		SpringApplication.run(Backend2InventoryDbApplication.class, args);
 	}
